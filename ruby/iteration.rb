8984f9bdf85047d617c9dc41b938e7005e29fff9
kimberlyjoyceembry <kimberly_embry@yahoo.com>
2016-10-18 14:01:49 -0700

    5.3 PS with Nick Devlin

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,58 +1,67 @@
+# Worked with Nick Devlin
+
 # Release 0
 def challenge53
 	name1 = "Nick"
-	name2 = "Kimberly
-	puts "Who is working on this project?""
+	name2 = "Kimberly"
+	puts "Who is working on this project?"
 	yield
 end 
 
-challenge53 { |name 1, name 2| puts "#{name1} and #{name2} are working on this project."  }
+challenge53 { |name1, name2| puts "#{name1} and #{name2} are working on this project."  }
 
 
-# Release 1 Video 1 
-letters = ["a", "b", "c", "d", "e"]
+ceo = {
+	"Twitter" => "Jack Dorsey", 
+	"Slack" => "Stewart Butterfield",
+	"SpaceX" => "Elon Musk"}
 
-puts "Original data:"
-p letters
-p new_letters
-
-# iterate through the array with .each
-# .each is good for looping through and printing, but usually not best for modifying an array
-# the best method for modifiy an array is .map
-letters.each do |letter|
-	new_letters << letter.next
+ceo.each do |company, executive|
+	puts "#{executive} is the CEO of #{company}."
 end
 
-
-puts "After .each call:"
-p letters
-p new_letters
-
-####################
-
-numbers = {1 => 'one', 2 => 'two'. 3 =>'three'}
-
-# iterate through the hash with .each
-
-numbers.each do |digit, word|
-	puts "#{digit} is spelled out as #{word}"
-end
-
-
-# Release 1 Video 2 .map and .map!
-letters = ["a", "b", "c", "d", "e"]
+ceo = ["Jack Dorsey", "Stewart Butterfield", "Elon Musk"]
 
 puts "Original data:"
-p letters
+p ceo
 
-# iterate through the array with .map
-letters.map do |letter|
-	puts letter
-	letter.next
-	"Hi!"
+ceo.map! do |executive|
+	executive.next 
 end
 
 puts "After .map call:"
-p letters
+p ceo
 
+# RELEASE 2: USE THE DOCUMENTATION
+
+# Array and method objectives:
+# A method that iterates through the items, deleting any that meet a certain condition
+# A method that filters a data structure for only items that do satisfy a certain condition 
+# A different method that filters a data structure for only items satisfying a certain condition
+# A method that will remove items from a data structure until the condition in the block evaluates to false, then stops
+
+# Array Examples
+north_america = ["usa", "mexico", "canada", "dominican republic"]
+
+p north_america.delete_if {|country| country.length < 4 }
+
+p north_america.select {|country| country.length > 8 }
+
+p north_america.reject {|country| country.length != 6 }
+
+p north_america.drop_while {|country| country.length < 3 }
+
+# Hash Examples
+state_caps = {
+	illinois: "springfield",
+	california: "sacramento",
+	texas: "austin",
+	iowa: "des_moines"
+}
+
+p state_caps.delete_if {|state, city| state.length == city.length }
+
+p state_caps.select {|state, city| state.length < city.length }
+
+p state_caps.reject {|state, city| city.length != 10 }
 
