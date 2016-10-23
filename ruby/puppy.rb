f0b0f151c8bd421226bf1e611e3805c9a4e4e4c1
kimberlyjoyceembry <kimberly_embry@yahoo.com>
2016-10-23 13:28:01 -0700

    6.1

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+# SPECIES --------------------
+# Puppy
+
+# CHARACTERISTICS ------------
+# Size: varies
+# Color: varies
+# Eye count: 2
+# Name: varies
+# Friendly: yes
+# Tail count: 1
+
+# BEHAVIOR -------------------
+# Bark
+# Explore
+# Lick 
