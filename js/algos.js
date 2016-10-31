b671d94c99c252256709792f77fe21b8eff6d03e
kimberly embry <kimberlyembry@kimberlys-MacBook-Air.local>
2016-10-30 22:15:26 -0700

    Finished 7.3 Solo Challenge generating random data

--- js/algos.js
+++ js/algos.js
@@ -45,3 +45,40 @@ function compare_two_objects(object_1, object_2) {
 console.log(compare_two_objects({month: "January", days: 31}, {month: "February", days: 28}));
 console.log(compare_two_objects({month: "March", days: 31}, {month: "April", days: 30}));
 console.log(compare_two_objects({month: "June", days: 30}, {month: "July", days: 30}))
+
+// RELEASE 2: Generate Random Test Data
+// Write a function that takes an integer for length
+// Create an empty array
+// Return the array with strings of the given length
+	// If you ran the function with an argument of 3, we would get an array of 3 "words" back
+	// The words should be of randomly varying length
+	// With a minimum of one number and a maximum of 10 letters
+		// To generate random number use dot random
+		// Create a string of variable for the alphabet for use as a key for letters
+// Add driver code that does the follow 10 times:
+	// Generates an array
+	// Prints the array
+	// Feed the array to your "longest word" function
+	// Prints the result
+function random_data(integer) {
+	var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+	var word = ""
+	var words = []
+	var x = 0
+	while (x < integer){
+		for(var i = 0; i <  Math.floor((Math.random() * 10) + 1); i++){
+			word += (alphabet[Math.floor((Math.random() * alphabet.length))]);
+		}
+		words.push(word);
+		word = ""
+		x++
+    }
+ 	return words
+	
+}
+
+// Driver Code
+console.log(random_data(10));
+var random_data_array = random_data(10);
+console.log(phrase(random_data_array))
+
