da14ab6c54d6e64775ca7c14050a55b1c07f8416
kimberly embry <kimberlyembry@kimberlys-MacBook-Air.local>
2016-11-03 19:34:24 -0700

    completed release 2 iterating over both hashes and arrays using different methods that take blocks

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -162,15 +162,44 @@ p fruit_colors
 
 # Release 2
 # Find other array and hash methods that take blocks to accomplish the below:
-	# 1. Iterates through items and deletes any that meet a certain condition
+# Declare an array and a hash
+# Do the following for each data structure (aka 8 times)
+	# 1. Iterates through items and deletes any that meet a certain condition (.delete_if)
 	# 2. Iterates through items and returns any that meet a certain condition
 	# 3. Iterates through items and returns any that meet a certain condition
 	# 4. Iterates through and deletes items in a data structure until the condition in the block evaluates to false
-	
+
+beer_brands = ["Blue Moon", "Corona", "Budweiser"]
+fruit_colors = {"bananas" => "yellow", "oranges" => "orange", "limes" => "green"}
 
 
+########### DRIVER CODE #############
 
+# Arrays
+# beer_brands.delete_if { |brand| brand == "Budweiser"}
+# p beer_brands
 
+# beer_brands.keep_if { |brand| brand == "Corona"}
+# p beer_brands
+
+# beer_brands.select! { |brand| brand == "Blue Moon"} # need to use bang operator to modify original data
+# p beer_brands
+
+# beer_brands.reject! { |brand| brand == "Budweiser"} # need to use bang operator to modify original data
+# p beer_brands
+
+# Hashes
+# fruit_colors.delete_if { |fruit, color| fruit == "limes"}
+# p fruit_colors # deletes both key and value that meets this condition
+
+# fruit_colors.keep_if { |fruit, color| fruit != "oranges"}
+# p fruit_colors
+
+# fruit_colors.select! { |fruit, color| color == "yellow"}
+# p fruit_colors
+
+# fruit_colors.reject! { |fruit, color| color == "yellow"}
+# p fruit_colors
 
 
 
